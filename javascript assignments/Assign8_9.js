233230c17bb1ac95fd54c38f751f239401053324
AdhikarShinde15 <88998172+AdhikarShinde15@users.noreply.github.com>
2021-08-31 18:36:58 +0530

    filter and rest assignments

---
+++ javascript assignments/Assign8_9.js
@@ -0,0 +1,14 @@
+//Assignment 8
+const randomNumbers = [14, 17, 13, 33, 37, 42, 5];
+
+const filterOdd = (randomNumbers.filter((ele) => (ele%2 == 0)))
+
+console.log(filterOdd)
+
+//Assignment 9
+
+const names = ["Anchal", "Adhikar", "Abhishek", "Zainab", "Anjali", "Manpreet", "Thrishma"];
+
+const [, myname] = names
+
+console.log(myname)
